a1a829c7eeffc6738e4162189b7e35a1ef642d97
Arthur Costa <artcosta1@gmail.com>
2025-07-07 22:24:54 -0300

    feat: add Terraform configuration for AWS ECR repository

---
+++ infra-aws/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.3.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_ecr_repository" "app_repository" {
+  name                 = var.ecr_repository_name
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
