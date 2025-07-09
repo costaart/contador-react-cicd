798054ddd77ad4caa0a2d60cce323a561d3c2759
Arthur Costa <artcosta1@gmail.com>
2025-07-08 21:22:43 -0300

    feat: change title

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="app">
       <div className="card">
-        <h1>🚀 Contador CI/CD - React PZM Talk</h1>
+        <h1>🚀 Contador CI/CD - React PZM Talk 2</h1>
         <p className="number">{count}</p>
         <div className="buttons">
           <button className="btn increment" onClick={() => setCount(count + 1)}>Incrementar + </button>
