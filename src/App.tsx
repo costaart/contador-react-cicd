efe417a9208cd1a33040bec339e2cda0d303f75b
Arthur Costa <artcosta1@gmail.com>
2025-07-09 14:24:57 -0300

    fix: changing title

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="app">
       <div className="card">
-        <h1>🚀 Contador CI/CD</h1>
+        <h1>🚀 Contador CI/CD - Tech Talk</h1>
         <p className="number">{count}</p>
         <div className="buttons">
           <button className="btn increment" onClick={() => setCount(count + 1)}>Incrementar + </button>
